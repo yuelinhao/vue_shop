d0c37dd107d0e2653dc6135acb6dded39116c6c9
yuelinhao <2062318011@qq.com>
2022-05-11 21:19:51 +0800

    登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ShopLogin from '../components/ShopLogin'
+import ShopHome from '../components/ShopHome'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: ShopLogin
+    },
+    {
+      path: '/home',
+      component: ShopHome
+    }
+  ]
 })
-
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
